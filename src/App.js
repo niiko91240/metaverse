1fcb5c3d62cc2deaf489f9b91cb363193ca821bc
Nicolas Anjorand <nicolas.ajrd@gmail.com>
2024-07-03 14:07:04 +0200

    add stormtrooper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import "aframe";
+import universe from  "./stormtrooper.glb";
+import stars from "./stars.webp"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
 
 function App() {
+  const loader = new GLTFLoader();
+  loader.load(universe, (d) => {
+    const entity = document.getElementById("universe");
+    entity.object3D.add(d.scene);
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <a-scene>
+      <a-assets>
+        <img id="sky" src={stars} alt="" />
+      </a-assets>
+      <a-entity id="universe" position="0 0 0" scale="15 15 15"  ></a-entity>
+      <a-light type="ambient" color="#445451"></a-light>
+      <a-light type="directional" intensity="0.5" position="2 4 5"></a-light>
+      <a-sky color="FFFFFF" material="src:#sky" rotation="0 0 0"></a-sky>
+    </a-scene>
   );
 }
 
